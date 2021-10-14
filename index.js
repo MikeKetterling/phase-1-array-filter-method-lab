e576876f65ba62a92da6fd0b4c6657b215aa128a
Mike <mikekett4@yahoo.com>
2021-10-14 16:31:52 -0600

    commit tree b8f5be46382ca82224880c72b6993b20e0a07540

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// const findMatching = (arr, string) => arr.filter(string) 
+
+function findMatching(arr, string) {
+    const result = arr.filter(newArr => newArr.toUpperCase() === string.toUpperCase())
+    return result
+}
+
+const fuzzyMatch = (driversArray, string) => {
+    const driverName = driversArray.filter(newArr => newArr.slice(0, string.length) === string)
+    return driverName
+}
+
+const matchName = (arr, string) => {
+    const dName = arr.filter(element => element.name === string)
+    return dName
+}
